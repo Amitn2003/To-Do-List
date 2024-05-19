bbc2201d560e9254baabdbf8a30cf2d2c873698d
Amit Naskar <amitntemp@gmail.com>
2024-05-20 01:53:44 +0530

    Update page bug solved

--- edit.php
+++ edit.php
@@ -30,44 +30,10 @@ $row = mysqli_fetch_array($rawData);
                 <input type="text" name="id" id="id" placeholder="Enter your task here..." value="<?php echo $row['id']; ?>" hidden>
                 <input type="submit" id="submitBtn" value="Edit Task">
             </form>
-            <div>
-                <ul class="taskList" id="taskList">
-                    <!-- <li><img src="checkbox-icon.svg" alt="" srcset="">Task Example 1111<span>X</span></li> -->
-                    <?php
-                    //$rawData = mysqli_query($con, "SELECT * FROM todo");
-                        while($row = mysqli_fetch_array($rawData)){
-                    ?>
-                    <li>
-                        <span><a href="delete.php?id=<?php echo $row['id'] ?>"><i class="fa-solid fa-check"></i></a></span>
-                        <span hidden>
-                            <?php echo $row['id'] ?>
-                        </span>
-                        <span>
-                            <?php echo $row['task'] ?>
-                        </span>
-                        
-                            <!-- <?php echo $row['time'] ?> -->
-                            <!-- </td> -->
-                            <span><a href="edit.php?id=<?php echo $row['id'] ?>"><i class="fa-regular fa-pen-to-square"></i></a></span>
-                            <!-- <span><a href="delete.php?id=<?php echo $row['id'] ?>">Delete</a></span> -->
-                    </li>
-                    <?php
-                    }   
-                    ?>
-                </ul>
-            </div>
+            
         </div>
     </div>
-    <main>
-        <div class="container">
-            <form action="update1.php" method="post">
-                <input type="text" name="task" id="task" placeholder="Enter your task here..." value="<?php echo $row['task']; ?>" autofocus >
-                
-                <input type="text" name="id" id="id" placeholder="Enter your task here..." value="<?php echo $row['id']; ?>" hidden>
-                <input type="submit" value="Update Task">
-            </form>
-        </div>
-    </main>
+    
     <!-- Get the data -->
     
 </body>
